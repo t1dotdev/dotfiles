bd5dd191e431d99cf39876bb9af7f460cea34819
Petchx <petchxpanuphong@gmail.com>
2024-04-12 01:44:32 +0700

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -205,7 +205,7 @@ config.window_padding = {
 }
 
 config.text_background_opacity = 0.8
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 10
 config.window_background_opacity = 0.8
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"

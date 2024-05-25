4af38cb1d10378e14582803798a0c3579607d2b1
Petchx <petchxpanuphong@gmail.com>
2024-05-25 13:10:08 +0700

    feat: add file operations

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -211,4 +211,14 @@ return {
 			end
 		end,
 	},
+	{
+		"antosha417/nvim-lsp-file-operations",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-tree.lua",
+		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+	},
 }

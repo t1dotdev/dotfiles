ad6e413580fd8d916278d0b24e84653f679bf2ee
Petchx <petchxpanuphong@gmail.com>
2024-06-28 15:12:43 +0700

    feat: update

--- dot_config/sketchybar/plugins/executable_space.sh
+++ dot_config/sketchybar/plugins/executable_space.sh
@@ -9,8 +9,10 @@ source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 if [ $SELECTED = true ]; then
 	sketchybar --set $NAME background.drawing=on \
 		background.color=$ACCENT_COLOR \
-		label.color=$WHITE icon.color=$WHITE
+		label.color=$WHITE \
+		icon.color=$WHITE
 else
 	sketchybar --set $NAME background.drawing=off \
-		label.color=$WHITE icon.color=$WHITE
+		label.color=$WHITE \
+		icon.color=$WHITE
 fi

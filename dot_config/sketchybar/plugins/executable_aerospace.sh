20ad28df70618518e74d3e659710189ab2be7f10
Panuphong Burakitphachai <petchx@Panuphongs-MacBook-Air.local>
2024-08-17 19:41:35 +0700

    feat: update

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -4,7 +4,23 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 #
 #
-sketchybar --set $NAME icon=$(aerospace list-workspaces --focused)
+# apps=$(aerospace list-windows --workspace $(aerospace list-workspaces --focused) --format '%{app-name}')
+# icon_strip=" "
+# if [ "${apps}" != "" ]; then
+#   while read -r app; do
+#     icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+#   done <<<"${apps}"
+# else
+#   icon_strip=" —"
+# fi
+# sketchybar --set space.$space label="$icon_strip"
+
+sketchybar --set $NAME \
+  icon=$(aerospace list-workspaces --focused) \ 
+label="$icon_strip" \
+  label.y_offset=-1 \
+  label.padding_right=20 \
+  label.font="sketchybar-app-font:Regular:13.0"
 
 # if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
 #   sketchybar --set $NAME background.drawing=on

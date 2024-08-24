4c3278bc939ff0053538e497dbbc464a00897ae7
Petchx <petchxpanuphong@gmail.com>
2024-08-24 10:45:49 +0700

    feat: update

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -4,13 +4,14 @@ return {
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
 			vim.list_extend(opts.ensure_installed, {
-				"luacheck",
-				"shellcheck",
-				"shfmt",
+				-- "luacheck",
+				-- "shellcheck",
+				-- "shfmt",
 				"tailwindcss-language-server",
 				"typescript-language-server",
 				"css-lsp",
-				"eslint-lsp",
+				-- "eslint-lsp",
+				"prettier",
 			})
 		end,
 	},

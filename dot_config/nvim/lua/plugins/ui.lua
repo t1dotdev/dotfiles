b4d9d20dcd4e1def2455137401619deee8058791
Panuphong Burakitphachai <petchx@Panuphongs-MacBook-Air.local>
2024-09-03 12:39:52 +0700

    feat: update

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -61,15 +61,15 @@ return {
 	},
 
 	-- animations
-	{
-		"echasnovski/mini.animate",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.scroll = {
-				enable = false,
-			}
-		end,
-	},
+	-- {
+	-- 	"echasnovski/mini.animate",
+	-- 	event = "VeryLazy",
+	-- 	opts = function(_, opts)
+	-- 		opts.scroll = {
+	-- 			enable = false,
+	-- 		}
+	-- 	end,
+	-- },
 
 	-- buffer line
 	{
@@ -124,21 +124,112 @@ return {
 	-- statusline
 	{
 		"nvim-lualine/lualine.nvim",
-		opts = function(_, opts)
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local lualine = require("lualine")
+			local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 			local LazyVim = require("lazyvim.util")
-			opts.sections.lualine_c[4] = {
-				LazyVim.lualine.pretty_path({
-					length = 0,
-					relative = "cwd",
-					modified_hl = "MatchParen",
-					directory_hl = "",
-					filename_hl = "Bold",
-					modified_sign = "",
-					readonly_icon = " 󰌾 ",
-				}),
+
+			local colors = {
+				primary = "#905aff",
+				blue = "#65D1FF",
+				green = "#3EFFDC",
+				violet = "#FF61EF",
+				yellow = "#FFDA7B",
+				red = "#FF4A4A",
+				fg = "#c3ccdc",
+				bg = "#000000",
+				inactive_bg = "#2c3043",
 			}
+
+			local my_lualine_theme = {
+				normal = {
+					a = { bg = colors.primary, fg = colors.bg, gui = "bold" },
+					-- b = { bg = colors.bg, fg = colors.fg },
+					c = { fg = colors.fg },
+				},
+				insert = {
+					a = { bg = colors.primary, fg = colors.bg, gui = "bold" },
+					-- b = { bg = colors.bg, fg = colors.fg },
+					-- c = { bg = colors.bg, fg = colors.fg },
+					c = { fg = colors.fg },
+				},
+				visual = {
+					a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+					-- b = { bg = colors.bg, fg = colors.fg },
+					-- c = { bg = colors.bg, fg = colors.fg },
+					c = { fg = colors.fg },
+				},
+				command = {
+					a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+					-- b = { bg = colors.bg, fg = colors.fg },
+					-- c = { bg = colors.bg, fg = colors.fg },
+					c = { fg = colors.fg },
+				},
+				replace = {
+					a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+					-- b = { bg = colors.bg, fg = colors.fg },
+					-- c = { bg = colors.bg, fg = colors.fg },
+					c = { fg = colors.fg },
+				},
+				inactive = {
+					a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+					-- b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+					-- c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+					c = { fg = colors.fg },
+				},
+			}
+
+			-- configure lualine with modified theme
+			lualine.setup({
+				options = {
+					theme = my_lualine_theme,
+				},
+				sections = {
+					lualine_c = {
+						LazyVim.lualine.pretty_path({
+							length = 0,
+							relative = "cwd",
+							modified_hl = "MatchParen",
+							directory_hl = "",
+							filename_hl = "Bold",
+							modified_sign = "",
+							readonly_icon = " 󰌾 ",
+							color = { fg = colors.fg },
+						}),
+					},
+					lualine_x = {
+						{
+							lazy_status.updates,
+							cond = lazy_status.has_updates,
+							color = { fg = "#ff9e64" },
+						},
+						-- { "encoding" },
+						-- { "fileformat" },
+
+						{ "filetype" },
+					},
+				},
+			})
 		end,
 	},
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	opts = function(_, opts)
+	-- 		local LazyVim = require("lazyvim.util")
+	-- 		opts.sections.lualine_c[4] = {
+	-- 			LazyVim.lualine.pretty_path({
+	-- 				length = 0,
+	-- 				relative = "cwd",
+	-- 				modified_hl = "MatchParen",
+	-- 				directory_hl = "",
+	-- 				filename_hl = "Bold",
+	-- 				modified_sign = "",
+	-- 				readonly_icon = " 󰌾 ",
+	-- 			}),
+	-- 		}
+	-- 	end,
+	-- },
 
 	{
 		"folke/zen-mode.nvim",

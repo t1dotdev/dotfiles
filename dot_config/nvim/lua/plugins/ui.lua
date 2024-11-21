7f59e4eba823b50488c0fc0265809c736a7fbfba
Petchx <petchx@Panuphongs-MacBook-Air.local>
2024-11-21 17:56:25 +0700

    feat: update dashboard

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -245,25 +245,142 @@ return {
 	},
 
 	{
-		"nvimdev/dashboard-nvim",
-		-- enable = false,
-		event = "VimEnter",
-		opts = function(_, opts)
-			vim.api.nvim_command("highlight DashboardHeader guifg=#905aff gui=bold")
-			vim.api.nvim_command("highlight DashboardFooter guifg=#905aff gui=italic")
-			local logo = [[
-██████╗ ███████╗████████╗ ██████╗██╗  ██╗██╗  ██╗
-██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║  ██║╚██╗██╔╝
-██████╔╝█████╗     ██║   ██║     ███████║ ╚███╔╝ 
-██╔═══╝ ██╔══╝     ██║   ██║     ██╔══██║ ██╔██╗ 
-██║     ███████╗   ██║   ╚██████╗██║  ██║██╔╝ ██╗
-╚═╝     ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝
-		    ]]
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				pane_gap = 6,
+				sections = {
+					{ section = "header", padding = 2 },
+					{ pane = 1, section = "keys", gap = 1, padding = 1 },
+					{
+						pane = 2,
+						icon = " ",
+						title = "Files",
+						padding = 1,
+					},
+					{
+						pane = 2,
+						-- icon = " ",
+						-- title = "Recent Files",
+						section = "recent_files",
+						indent = 3,
+						padding = 2,
+					},
+					{
+						pane = 2,
+						icon = " ",
+						title = "Projects",
+						padding = 1,
+					},
+					{ pane = 2, section = "projects", indent = 3, padding = 2 },
+					-- {
+					-- 	pane = 2,
+					-- 	section = "startup",
+					-- },
+					-- {
+					-- 	pane = 2,
+					-- 	icon = " ",
+					-- 	title = "Git Status",
+					-- 	section = "terminal",
+					-- 	enabled = vim.fn.isdirectory(".git") == 1,
+					-- 	cmd = "git status --short --branch --renames",
+					-- 	height = 5,
+					-- 	padding = 1,
+					-- 	ttl = 5 * 60,
+					-- 	indent = 3,
+					-- },
+				},
+				preset = {
+					-- 					header = [[
+					--         tGCG1   1GCGt
+					--         .L@@@f,f@@@L.
+					--          .8@@@1@@@8.
+					--         :8@@0: :0@@8:           ██╗    ██╗██╗████████╗███████╗██╗      █████╗ ██████╗
+					--        i@@@G.   .G@@@i          ██║    ██║██║╚══██╔══╝██╔════╝██║     ██╔══██╗██╔══██╗
+					--       t@@@L       L@@@t         ██║ █╗ ██║██║   ██║   █████╗  ██║     ███████║██████╔╝
+					--      L@@@t   i0i   t@@@L        ██║███╗██║██║   ██║   ██╔══╝  ██║     ██╔══██║██╔══██╗
+					--    .C@@@i   t@@@t   i@@@C.      ╚███╔███╔╝██║   ██║   ███████╗███████╗██║  ██║██████╔╝
+					--   ,0@@8i ,,t@888@t,, i8@@0,      ╚══╝╚══╝ ╚═╝   ╚═╝   ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝
+					--   0@@@@@@0;,.....,;0@@@@@@0
+					--   ;LCCGGC:         :CGGCCL;
+					-- ]],
+					-- 					header = [[
+					--       tGCG1   1GCGt
+					--       .L@@@f,f@@@L.
+					--        .8@@@1@@@8.
+					--       :8@@0: :0@@8:             oooo     oooo o88    o8              ooooo                   oooo
+					--      i@@@G.   .G@@@i             88   88  88  oooo o888oo ooooooooo8  888          ooooooo    888ooooo
+					--     t@@@L       L@@@t             88 888 88    888  888  888oooooo8   888          ooooo888   888    888
+					--    L@@@t   i0i   t@@@L              888 888     888  888  888          888      o 888    888   888    888
+					--  .C@@@i   t@@@t   i@@@C.              8   8     o888o  888o  88oooo888 o888ooooo88  88ooo88 8o o888ooo88
+					-- ,0@@8i ,,t@888@t,, i8@@0,
+					-- 0@@@@@@0;,.....,;0@@@@@@0
+					-- ;LCCGGC:         :CGGCCL;
+					--     ]],
+					header = [[
+	██████╗ ███████╗████████╗ ██████╗██╗  ██╗██╗  ██╗
+	██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║  ██║╚██╗██╔╝
+	██████╔╝█████╗     ██║   ██║     ███████║ ╚███╔╝
+	██╔═══╝ ██╔══╝     ██║   ██║     ██╔══██║ ██╔██╗
+	██║     ███████╗   ██║   ╚██████╗██║  ██║██╔╝ ██╗
+	╚═╝     ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝
+]],
 
-			logo = string.rep("\n", 8) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+{ icon = " ", key = "g", desc = "Lazygit", action = ":lua require('lazygit').lazygit()", enabled = vim.fn.isdirectory(".git") == 1},
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+				},
+			},
+		},
+		config = function(_, opts)
+			require("snacks").setup(opts)
+
+			-- Set highlight groups for Snacks Dashboard
+			vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#905aff", bold = true })
+			vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#ffffff", bold = true })
+			vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { fg = "#905aff", italic = true })
+			vim.api.nvim_set_hl(0, "SnacksDashboardIcon", { fg = "#ffffff", bold = true }) -- Pink for icons
+			vim.api.nvim_set_hl(0, "SnacksDashboardDesc", { fg = "#ffffff", italic = true })
+			vim.api.nvim_set_hl(0, "SnacksDashboardProjects", { fg = "#905aff", bold = true })
+
+			vim.api.nvim_set_hl(0, "SnacksDashboardTitle", { fg = "#ffffff", bold = true }) -- Gold color for titles
+			vim.api.nvim_set_hl(0, "SnacksDashboardIcon", { fg = "#905aff" }) -- Spring green for icons
+			vim.api.nvim_set_hl(0, "SnacksDashboardFile", { fg = "#ffffff", bold = true }) -- Gold color for files
+			vim.api.nvim_set_hl(0, "SnacksDashboardDir", { fg = "#905aff" }) -- Hot pink for directories
 		end,
 	},
+
+	-- 	{
+	-- 		"nvimdev/dashboard-nvim",
+	-- 		-- enable = false,
+	-- 		event = "VimEnter",
+	-- 		opts = function(_, opts)
+	-- 			vim.api.nvim_command("highlight DashboardHeader guifg=#905aff gui=bold")
+	-- 			vim.api.nvim_command("highlight DashboardFooter guifg=#905aff gui=italic")
+	-- 			local logo = [[
+	-- ██████╗ ███████╗████████╗ ██████╗██╗  ██╗██╗  ██╗
+	-- ██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║  ██║╚██╗██╔╝
+	-- ██████╔╝█████╗     ██║   ██║     ███████║ ╚███╔╝
+	-- ██╔═══╝ ██╔══╝     ██║   ██║     ██╔══██║ ██╔██╗
+	-- ██║     ███████╗   ██║   ╚██████╗██║  ██║██╔╝ ██╗
+	-- ╚═╝     ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝
+	-- 		    ]]
+	--
+	-- 			logo = string.rep("\n", 8) .. logo .. "\n\n"
+	-- 			opts.config.header = vim.split(logo, "\n")
+	-- 		end,
+	-- 	},
 	{
 		"kdheepak/lazygit.nvim",
 		keys = {

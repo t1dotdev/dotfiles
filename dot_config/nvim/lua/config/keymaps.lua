7b99c456ed291662bfea1e1d1ce7c77e2a58a5ec
Petchx <petchxpanuphong@gmail.com>
2024-11-29 19:44:19 +0700

    feat: update nvim keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -25,29 +25,31 @@ keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
 -- keymap.set("n", "dw", 'vb"_d')
---
 
 keymap.set("n", "<Leader>e", ":NvimTreeToggle<Return>", opts)
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
+-- Move vertically centered
 keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "<C-d>", "<C-d>zz")
+
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-keymap.set("n", "J", "mzJ`z")
-keymap.set("n", "n", "nzzv")
-keymap.set("n", "N", "Nzzv")
-keymap.set("n", "Y", "y$")
+
+-- keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
+-- keymap.set("n", "Y", "y$")
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 -- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+-- keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
 keymap.set("n", "te", ":tabedit")

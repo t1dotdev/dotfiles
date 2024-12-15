460b6083c74317795ce59e1dbe3f1ef6e7271099
Petchx <petchxpanuphong@gmail.com>
2024-12-15 10:06:17 +0700

    add toggle term

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -23,6 +23,23 @@ return {
 	},
 
 	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		cmd = "ToggleTerm",
+		keys = { { "<c-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+		opts = {
+			open_mapping = [[<c-\>]],
+			direction = "float",
+			shade_filetypes = {},
+			hide_numbers = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			start_in_insert = true,
+			close_on_exit = true,
+		},
+	},
+
+	{
 		"ibhagwan/fzf-lua",
 		cmd = "FzfLua",
 		opts = function(_, opts)

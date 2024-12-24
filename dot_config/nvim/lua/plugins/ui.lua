c66367e2a5312027ad0b55769382c058745f7694
Petchx <petchxpanuphong@gmail.com>
2024-12-24 14:42:07 +0700

    update nvim

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -15,8 +15,8 @@ return {
 			options = {
 				mode = "tabs",
 				-- separator_style = "slant",
-				show_buffer_close_icons = false,
-				show_close_icon = false,
+				-- show_buffer_close_icons = false,
+				-- show_close_icon = false,
 			},
 		},
 	},

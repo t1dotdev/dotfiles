84e15cb291b4ebad48851e85beb47f8bbf4fb8c7
T1 <topone@Topones-Mac-mini.local>
2025-01-29 11:03:52 +0700

    update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -13,7 +13,7 @@ vim.g.snacks_animate = false
 -- Can be one of: telescope, fzf
 -- Leave it to "auto" to automatically use the picker
 -- enabled with `:LazyExtras`
-vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_picker = "auto"
 
 -- LazyVim completion engine to use.
 -- Can be one of: nvim-cmp, blink.cmp

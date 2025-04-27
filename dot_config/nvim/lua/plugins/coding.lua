d1259d86c0fb119b17114f2a83f1b8b0c18258db
t1dotdev <petchxpanuphong@gmail.com>
2025-04-27 13:51:23 +0700

    update configs

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -188,7 +188,9 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		config = true,
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
 	},
 	{
 		"echasnovski/mini.pairs",

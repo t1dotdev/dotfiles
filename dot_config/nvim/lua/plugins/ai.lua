98b54f1d5370651d44334fe495fee904dd223746
t1dotdev <petchxpanuphong@gmail.com>
2025-04-28 11:34:21 +0700

    use codecompanion

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,1 +1,26 @@
-return {}
+vim.keymap.set({ "n", "v", "x" }, "<leader>aa", "<cmd>CodeCompanionAction<CR>", { desc = "[A]i [A]ction" })
+
+return {
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("codecompanion").setup({
+				adapters = {
+					copilot = function()
+						return require("codecompanion.adapters").extend("copilot", {
+							schema = {
+								model = {
+									default = "gpt-4o-mini",
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
+}

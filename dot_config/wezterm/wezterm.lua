fd8234d327b49b518bf7b327b0d996c3b9e06053
t1dotdev <petchxpanuphong@gmail.com>
2025-05-07 20:01:44 +0700

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -83,77 +83,77 @@ wezterm.on("format-tab-title", function(tab, _, _, _, _, max_width)
 end)
 
 -- === Right Status Bar ===
-wezterm.on("update-right-status", function(window, pane)
-	local cells = {}
-	-- table.insert(cells, wezterm.nerdfonts.md_apple)
-
-	local cwd_uri = pane:get_current_working_dir()
-	if cwd_uri then
-		local cwd, hostname = "", ""
-		if type(cwd_uri) == "userdata" then
-			cwd = cwd_uri.file_path
-			hostname = cwd_uri.host or wezterm.hostname()
-		else
-			cwd_uri = cwd_uri:sub(8)
-			local slash = cwd_uri:find("/")
-			if slash then
-				hostname = cwd_uri:sub(1, slash - 1)
-				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
-					return string.char(tonumber(hex, 16))
-				end)
-			end
-		end
-		local dot = hostname:find("[.]")
-		if dot then
-			hostname = hostname:sub(1, dot - 1)
-		end
-		if hostname == "" then
-			hostname = wezterm.hostname()
-		end
-		table.insert(cells, cwd)
-	end
-
-	local handle = io.popen("ipconfig getifaddr en0 || ipconfig getifaddr en1")
-	local local_ip = handle:read("*a"):gsub("\n", "")
-	handle:close()
-	-- table.insert(cells, wezterm.nerdfonts.cod_globe .. " " .. local_ip)
-
-	for _, b in ipairs(wezterm.battery_info()) do
-		local icons = {
-			wezterm.nerdfonts.md_battery_10,
-			wezterm.nerdfonts.md_battery_20,
-			wezterm.nerdfonts.md_battery_30,
-			wezterm.nerdfonts.md_battery_40,
-			wezterm.nerdfonts.md_battery_50,
-			wezterm.nerdfonts.md_battery_60,
-			wezterm.nerdfonts.md_battery_70,
-			wezterm.nerdfonts.md_battery_80,
-			wezterm.nerdfonts.md_battery_90,
-			wezterm.nerdfonts.md_battery,
-			wezterm.nerdfonts.md_battery_charging,
-		}
-		local level = math.floor(b.state_of_charge * 10)
-		local icon = b.state == "Charging" and icons[11] or icons[level]
-		if b.state ~= "Empty" then
-			table.insert(cells, icon .. " " .. string.format("%.0f%%", b.state_of_charge * 100))
-		end
-	end
-
-	local text_fg, bg = "#121212", colors.primary
-	local elements, count = {}, 0
-	local function push(text, is_last)
-		count = count + 1
-		table.insert(elements, { Foreground = { Color = text_fg } })
-		table.insert(elements, { Background = { Color = bg } })
-		table.insert(elements, { Text = " " .. text .. " " })
-	end
-
-	while #cells > 0 do
-		push(table.remove(cells, 1), #cells == 0)
-	end
-
-	window:set_right_status(wezterm.format(elements))
-end)
+-- wezterm.on("update-right-status", function(window, pane)
+-- 	local cells = {}
+-- 	-- table.insert(cells, wezterm.nerdfonts.md_apple)
+--
+-- 	local cwd_uri = pane:get_current_working_dir()
+-- 	if cwd_uri then
+-- 		local cwd, hostname = "", ""
+-- 		if type(cwd_uri) == "userdata" then
+-- 			cwd = cwd_uri.file_path
+-- 			hostname = cwd_uri.host or wezterm.hostname()
+-- 		else
+-- 			cwd_uri = cwd_uri:sub(8)
+-- 			local slash = cwd_uri:find("/")
+-- 			if slash then
+-- 				hostname = cwd_uri:sub(1, slash - 1)
+-- 				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+-- 					return string.char(tonumber(hex, 16))
+-- 				end)
+-- 			end
+-- 		end
+-- 		local dot = hostname:find("[.]")
+-- 		if dot then
+-- 			hostname = hostname:sub(1, dot - 1)
+-- 		end
+-- 		if hostname == "" then
+-- 			hostname = wezterm.hostname()
+-- 		end
+-- 		table.insert(cells, cwd)
+-- 	end
+--
+-- 	local handle = io.popen("ipconfig getifaddr en0 || ipconfig getifaddr en1")
+-- 	local local_ip = handle:read("*a"):gsub("\n", "")
+-- 	handle:close()
+-- 	-- table.insert(cells, wezterm.nerdfonts.cod_globe .. " " .. local_ip)
+--
+-- 	for _, b in ipairs(wezterm.battery_info()) do
+-- 		local icons = {
+-- 			wezterm.nerdfonts.md_battery_10,
+-- 			wezterm.nerdfonts.md_battery_20,
+-- 			wezterm.nerdfonts.md_battery_30,
+-- 			wezterm.nerdfonts.md_battery_40,
+-- 			wezterm.nerdfonts.md_battery_50,
+-- 			wezterm.nerdfonts.md_battery_60,
+-- 			wezterm.nerdfonts.md_battery_70,
+-- 			wezterm.nerdfonts.md_battery_80,
+-- 			wezterm.nerdfonts.md_battery_90,
+-- 			wezterm.nerdfonts.md_battery,
+-- 			wezterm.nerdfonts.md_battery_charging,
+-- 		}
+-- 		local level = math.floor(b.state_of_charge * 10)
+-- 		local icon = b.state == "Charging" and icons[11] or icons[level]
+-- 		if b.state ~= "Empty" then
+-- 			table.insert(cells, icon .. " " .. string.format("%.0f%%", b.state_of_charge * 100))
+-- 		end
+-- 	end
+--
+-- 	local text_fg, bg = "#121212", colors.primary
+-- 	local elements, count = {}, 0
+-- 	local function push(text, is_last)
+-- 		count = count + 1
+-- 		table.insert(elements, { Foreground = { Color = text_fg } })
+-- 		table.insert(elements, { Background = { Color = bg } })
+-- 		table.insert(elements, { Text = " " .. text .. " " })
+-- 	end
+--
+-- 	while #cells > 0 do
+-- 		push(table.remove(cells, 1), #cells == 0)
+-- 	end
+--
+-- 	window:set_right_status(wezterm.format(elements))
+-- end)
 
 -- === Smart Splits ===
 smart_splits.apply_to_config(config, {

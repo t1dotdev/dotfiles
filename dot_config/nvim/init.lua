4948fc20978220c1d002d6be3be5749bf9377fef
t1dotdev <petchxpanuphong@gmail.com>
2025-05-08 10:50:09 +0700

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", fg = "#875fff" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "#875fff" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE", fg = "#875fff" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#875fff", fg = "#16161e" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#875fff" })
+
+vim.api.nvim_set_hl(0, "BlinkCmpLabelMatch", { bg = "NONE", fg = "#875fff" })
+vim.api.nvim_set_hl(0, "BlinkCmpLabelDescription", { bg = "NONE", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE", ctermbg = "NONE" })

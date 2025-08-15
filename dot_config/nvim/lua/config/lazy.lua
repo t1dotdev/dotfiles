8cf170b7367114cf6a7021537bd8fda94cec85d5
t1dotdev <topone@topones-MacBook-Air.local>
2025-08-15 17:18:34 +0700

    update theme + font

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "sonokai",
+      colorscheme = "eldritch",
     } },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },

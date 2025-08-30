7fd2b14810061d35d14f8d19fce41b52d288d175
t1dotdev <petchxpanuphong@gmail.com>
2025-08-30 21:21:06 +0700

    update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,13 @@
-return { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "html", "javascript", "typescript", "tsx", "xml" },
+			auto_install = true,
+			highlight = { enable = true },
+		})
+	end,
+}
--- dot_config/nvim/lua/plugins/ts-autotag.lua
+++ dot_config/nvim/lua/plugins/ts-autotag.lua
@@ -1,5 +1,14 @@
 return {
 	"windwp/nvim-ts-autotag",
 	event = { "BufReadPost", "BufNewFile" },
-	opts = {},
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = true,
+			},
+		})
+	end,
 }
